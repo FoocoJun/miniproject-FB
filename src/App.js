50272f05894e9247cd73ca105f6ca59d997743cf
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-07-22 23:51:38 +0900

    MainCard !

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppMain>
+      <MainCard>
+      </MainCard>
+    </AppMain>
   );
 }
+
+const AppMain = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const MainCard = styled.div`
+  background-color: #fff;
+  border-radius: 40px;
+  width: 400px;
+  height: 568.89px;
+  @media (max-width: 500px) {
+    & {
+      width: 300px;
+      height: 426.67px;
+    }
+  }
+`;
 
 export default App;
