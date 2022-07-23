7a92ee13862af3a2743efc256dbd3532189d2342
인소현 <sohyun7305@naver.com>
2022-07-23 20:21:00 +0900

    PostPage Add

--- src/pages/SelectPage.js
+++ src/pages/SelectPage.js
@@ -3,18 +3,34 @@ import React from "react";
 import styled from "styled-components";
 
 const SelectPage = () => {
-    return ( 
-        <div>
-            <Title> 선택하시오 </Title>
-        </div>
-    );
+  return (
+    <div style={{width:"100%", overflow:"hidden"}}>
+      <Title> 선택하시오 </Title>
+      <ImgBox>
+        <div></div>
+        <div></div>
+        
+      </ImgBox>
+    </div>
+  );
 };
-
 
 // Stlyed Components 적용
 
 const Title = styled.h1`
-    color: #440068;
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: center;
+
+  font-family: "PyeongChangPeace-Bold";
+  text-align: right;
+  color: #440068;
+`;
+
+const ImgBox = styled.div`
+width: 400px;
+height: 400px;
+background-color: red;
 `;
 
 export default SelectPage;
--- src/pages/UserPages.js
+++ src/pages/UserPages.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
+import PostListPage from "./PostListPage";
 
 const UserPages = () => {
   return (
     <Routes>
       {/* 일기장 페이지 // 일기장 : 소현 */}
-      <Route path="list" element={null} />
+      <Route path="list" element={<PostListPage />} />
       {/* 잘못 된 접근 페이지 */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
