e48e99f9241a0fd5e342cf27e467af99d6423dc6
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-07-26 00:27:53 +0900

    Image --

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -2,32 +2,36 @@ import React from "react";
 
 import styled from "styled-components";
 
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const PostPage = () => {
-    const navigate = useNavigate();
-    console.log(document.body)    
+  const navigate = useNavigate();
+  console.log(document.body);
 
   return (
     <ComponentBox>
-        <Title> 밤의 이야기 </Title>
-            <PostBox>
+      <Title> 밤의 이야기 </Title>
+      <PostBox>
+        <FortuneTextBox>
+          {/* 운세글 get */}
+          <FortuneText> 아침에 옆사람 뒷통수를 때리면 즐거울거야. </FortuneText>
+        </FortuneTextBox>
 
-                <FortuneTextBox>
-                    {/* 운세글 get */}
-                    <FortuneText> 아침에 옆사람 뒷통수를 때리면 즐거울거야. </FortuneText>
-                </FortuneTextBox>
+        <InputStyle
+          rows="3"
+          cols="20"
+          wrap="hard"
+          placeholder="내용을 입력하세요."
+        />
 
-                <InputStyle rows="3" cols="20" wrap="hard"  placeholder="내용을 입력하세요."/>
-
-                {/* 작성 버튼 */}
-                <MediumButton onClick={() => navigate("/user/list")}> 작성하기 </MediumButton>
-
-            </PostBox>
-            
+        {/* 작성 버튼 */}
+        <MediumButton onClick={() => navigate("/user/list")}>
+          작성하기
+        </MediumButton>
+      </PostBox>
     </ComponentBox>
-  )
-}
+  );
+};
 
 const ComponentBox = styled.div`
   display: flex;
@@ -43,35 +47,35 @@ const Title = styled.h1`
   color: #440068;
 `;
 const PostBox = styled.form`
-    width: 75%;
-    height: 80%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  width: 75%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 const FortuneTextBox = styled.div`
-    width: 70%;
-    height: 30%;
+  width: 70%;
+  height: 30%;
 
-    color: #10305F;
+  color: #10305f;
 `;
 const FortuneText = styled.h3`
-    font-size: 1rem;
-    text-align: center;
-    font-family: '국립박물관문화재단클래식B';
-    word-break:keep-all;
+  font-size: 1rem;
+  text-align: center;
+  font-family: "국립박물관문화재단클래식B";
+  word-break: keep-all;
 `;
 const InputStyle = styled.textarea`
-    width: 100%;
-    height: 70%;
-    border: 1px solid #eee;
-    box-shadow: -2px 4px 5px rgb(189, 189, 189);
-    border-radius: 2rem;
-    box-sizing: border-box;
-    padding: 10%;
-    resize: none;
-    margin-bottom: 8%;
+  width: 100%;
+  height: 70%;
+  border: 1px solid #eee;
+  box-shadow: -2px 4px 5px rgb(189, 189, 189);
+  border-radius: 2rem;
+  box-sizing: border-box;
+  padding: 10%;
+  resize: none;
+  margin-bottom: 8%;
 `;
 const MediumButton = styled.button`
   width: 80%;
@@ -82,6 +86,5 @@ const MediumButton = styled.button`
   color: #fff;
   border-radius: 20px;
 `;
-
 
 export default PostPage;
