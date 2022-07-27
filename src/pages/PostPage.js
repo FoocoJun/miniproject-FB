306242384a9e7fba4d95fdb0383b0d393b089c01
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-07-27 10:49:25 +0900

    Proxy++

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -3,6 +3,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const PostPage = () => {
   const navigate = useNavigate();
